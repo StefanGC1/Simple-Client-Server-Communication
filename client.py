0b3d689a214593d3731e351a6d65cb88c7743021
StefanGC1 <george.crisan03@e-uvt.ro>
2024-03-17 19:10:21 +0200

    First commit

---
+++ client.py
@@ -0,0 +1,42 @@
+import socket
+
+
+def main():
+    # Some constants
+    host = '127.0.0.1'
+    port = 12345
+    client_id = None
+
+    # Creating the socket and connecting to the server (address)
+    client_socket = socket.socket()
+    client_socket.connect((host, port))
+    print(f"[CLIENT] Client connected to server on {host}:{port}.")
+
+    # Receive the id
+    client_id = int(client_socket.recv(1024).decode())
+    print(f"[CLIENT_{client_id}] Client id given by server: {client_id}")
+
+    while True:
+        try:
+            # Send data from input to the server
+            data_to_send = input("[SYSTEM] Enter data to send to server (enter exit to quit): ")
+
+            if data_to_send.lower() == 'exit':
+                break
+
+            client_socket.sendall(data_to_send.encode())
+
+            received_data = client_socket.recv(1024)
+            print(f"[CLIENT_{client_id}] Server received and sent back following data: {received_data.decode()}")
+        except ConnectionAbortedError:
+            # Handle server shutdown
+            print("\n[CLIENT] Server closed down!")
+            break
+
+
+    client_socket.close()
+    print(f"[CLIENT_{client_id}] Connection closed!")
+
+
+if __name__ == '__main__':
+    main()
---
+++ server.py
@@ -0,0 +1,82 @@
+import socket
+import threading
+
+# A sort of flag to indicate when the server is in the process of closing
+shutdown_event = threading.Event()
+
+
+def handle_client(conn, addr, client_id):
+    print(f"[SERVER_THREAD] Connected to CLIENT_{client_id} on {addr}")
+    conn.settimeout(1.0)
+
+    if not shutdown_event.is_set():
+        conn.sendall(str(client_id).encode())
+
+    while not shutdown_event.is_set():
+        try:
+            # Sets a 1-second limit on how long the server can wait for receiving data
+            data = conn.recv(1024)
+            if not data:
+                break
+            # Checks the flag to see if server is shutting down
+            if shutdown_event.is_set():
+                print(f"[SERVER_THREAD] Shutdown in progress. Closing connection with CLIENT_{client_id}")
+                break
+            # Receive data and send it back to the client
+            print(f"[SERVER_THREAD] Received data from CLIENT_{client_id}: {data.decode()}")
+            conn.sendall(data)
+        except socket.timeout:
+            continue
+        except ConnectionResetError:
+            break
+
+    print(f"[SERVER_THREAD] CLIENT_{client_id} disconnected")
+    conn.close()
+
+
+def main():
+    # Some constants
+    client_counter = 0
+    host = '127.0.0.1'
+    port = 12345
+
+    # Creating the socket and linking an address to it
+    server_socket = socket.socket()
+    server_socket.bind((host, port))
+
+    try:
+        # Sets the server into a state to listen for connections
+        server_socket.listen()
+        print(f"[SERVER] listening on {host}:{port}.")
+
+        # Sets a 1-second limit on how long the server can wait for a connection
+        server_socket.settimeout(1.0)
+
+        while not shutdown_event.is_set():
+            try:
+                # Wait for a connection
+                conn, addr = server_socket.accept()
+            except socket.timeout:
+                continue
+
+            # When a connection is established, increase client_counter and create a thread that handles
+            # the current client
+            client_counter += 1
+            client_thread = threading.Thread(target=handle_client, args=(conn, addr, client_counter))
+            client_thread.start()
+            print(f"\n[SERVER] Active connections: {threading.active_count() - 1}")
+    except KeyboardInterrupt:
+        # Handle a shutdown event
+        print("\n[SERVER] Shutdown signal received. Shutting down...")
+        shutdown_event.set()
+
+    server_socket.close()
+
+    # Wait for client threads to finish
+    for thread in threading.enumerate():
+        if thread is not threading.main_thread():
+            thread.join()
+
+
+if __name__ == '__main__':
+    main()
